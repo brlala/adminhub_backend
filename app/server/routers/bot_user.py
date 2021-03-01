cde1e8a3f6586157df6783ad3fecf19121130c63
liheng <liheng@pand.ai>
2021-03-01 19:03:58 +0800

    change api method

--- app/server/routers/bot_user.py
+++ app/server/routers/bot_user.py
@@ -1,11 +1,7 @@
 from fastapi import APIRouter, Query
 
-from fastapi import APIRouter, Query
-
 from ..db_utils.bot_user import get_bot_user_db, update_bot_user_db
-from ..db_utils.conversations import get_user_message_conversations_and_count_db, get_convo_conversations_and_count_db
 from ..models.bot_user import BotUserSchemaDb
-from ..models.message import GetMessagesTable
 
 router = APIRouter(
     tags=["Bot User"],
@@ -19,7 +15,7 @@ async def get_bot_user(bot_user_id: str):
     return await get_bot_user_db(bot_user_id)
 
 
-@router.put("/{bot_user_id}")
+@router.patch("/{bot_user_id}")
 async def update_bot_user(bot_user_id: str, tags: list[str] = Query([])):
     status = await update_bot_user_db(bot_user_id, tags=tags)
     return {
