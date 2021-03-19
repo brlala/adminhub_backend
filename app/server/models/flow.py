e0397ce796d9b580b53aaf63d710a145147fe071
kaidi <kaidi@pand.ai>
2021-03-19 15:27:01 +0800

    update flow type

--- app/server/models/flow.py
+++ app/server/models/flow.py
@@ -173,12 +173,12 @@ class FlowComponents(AttachmentItemComponent, GenericTemplateComponent, TextComp
 
 class FlowTypeEnum(str, Enum):
     GENERIC_TEMPLATE = 'genericTemplate'
-    IMAGES = 'imageAttachment'
+    IMAGES = 'images'
     FILE = 'file'
     BUTTON_TEMPLATE = 'buttonTemplate'
     FLOW = 'flow'
     MESSAGE = 'message'
-    VIDEOS = 'videoAttachment'
+    VIDEOS = 'videos'
     INPUT = 'input'
     CUSTOM = 'custom'
     IMAGE = 'image'
@@ -809,9 +809,9 @@ class FlowTypeEnumOut(str, Enum):
 
     def __str__(self):
         if self.value == self.IMAGES:
-            return 'imageAttachment'
+            return 'images'
         elif self.value == self.VIDEOS:
-            return 'videoAttachment'
+            return 'videos'
         return stringcase.camelcase(self.value)
 
 
