1580e90c7eb78f17339cd3c02fd1a22a145894ff
liheng <liheng@pand.ai>
2021-03-22 15:42:28 +0800

    added grading apis

--- app/server/db_utils/grading.py
+++ app/server/db_utils/grading.py
@@ -1,14 +1,17 @@
+import uuid
 from datetime import date
+from re import escape
 
-from bson import ObjectId
+from bson import ObjectId, Regex
 
-from app.server.db.collections import message_collection as collection
+from app.server.db.collections import message_collection
+from app.server.db.collections import question_collection
 from app.server.db_utils.bot_user import get_bot_user_db
 from app.server.db_utils.flows import get_flow_one
 from app.server.db_utils.helper import message_helper
 from app.server.db_utils.questions import get_question_one
 from app.server.models.current_user import CurrentUserSchema
-from app.server.models.message import MessageGradingSchemaDb, SkipMessage
+from app.server.models.message import MessageGradingSchemaDb, SkipMessage, UpdateMessageResponse
 from app.server.utils.common import form_query
 from app.server.utils.timezone import get_local_datetime_now, make_timezone_aware
 
@@ -53,8 +56,8 @@ async def get_grading_messages_and_count_db(topic: str, search_query: str, accur
         ]
     query = form_query(db_key)
     sort = [("_id", -1)]
-    cursor = collection.find(query, sort=sort)
-    total = await collection.count_documents(query)
+    cursor = message_collection.find(query, sort=sort)
+    total = await message_collection.count_documents(query)
     cursor.skip((current_page - 1) * page_size).limit(page_size)
     messages = []
     async for message in cursor:
@@ -64,8 +67,15 @@ async def get_grading_messages_and_count_db(topic: str, search_query: str, accur
         except:
             message['fullname'] = 'User'
 
+        if qnid := message.get('adminportal', {}).get('answer'):
+            question = await get_question_one(qnid)
+            if question:
+                for a in question.answers:
+                    flow = await get_flow_one(a.flow['flow_id'])
+                    message['answer_question'] = question
+                    message['answer_flow'] = flow
         # flow
-        if qnid := message.get('chatbot', {}).get('qnid'):
+        elif qnid := message.get('chatbot', {}).get('qnid'):
             question = await get_question_one(qnid)
             if question:
                 for a in question.answers:
@@ -95,6 +105,60 @@ async def skip_message_db(message: SkipMessage, current_user: CurrentUserSchema)
         "adminportal.graded": True,
         "adminportal.answer": None
     }
-    result = await collection.update_one(query, {'$set': set_query})
+    result = await message_collection.update_one(query, {'$set': set_query})
 
     return f"Skipped {result.modified_count} question."
+
+
+async def update_message_db(message_item: UpdateMessageResponse, current_user: CurrentUserSchema,
+                            language: str = 'EN') -> str:
+    query = {"_id": ObjectId(message_item.id)}
+    result1 = result2 = result3 = 0
+
+    # add selected answer to message if it's not same with original response/graded response
+    message_from_db = await message_collection.find_one(query)
+    graded_response = message_from_db.get('adminportal', {}).get('answer')
+    original_response = message_from_db.get('chatbot', {}).get('qnid')
+
+    response = graded_response or original_response
+    if not graded_response and response == message_item.new_response:
+        return 'No questions updated'
+
+    updated_info_query = {
+        "updated_at": get_local_datetime_now(),
+        "updated_by": ObjectId(current_user.userId),
+    }
+    # add graded response to message
+    set_message_query = updated_info_query | {
+        "adminportal.graded": True,
+        "adminportal.answer": ObjectId(message_item.new_response)
+    }
+    result1 = await message_collection.update_one(query, {'$set': set_message_query})
+
+    # delete variation from main question and add variation to new question
+    query = {"_id": ObjectId(response),
+             "alternate_questions.text": Regex(f"^{escape(message_item.text)}$", "i"),
+             "is_active": True}
+    if question_db := await question_collection.find_one(query):  # remove variation if found match
+        for idx, v in enumerate(question_db['alternate_questions']):
+            if v['text'].lower() == message_item.text.lower():
+                question_db['alternate_questions'].pop(idx)
+                question_db |= updated_info_query
+                result2 = await question_collection.replace_one({"_id": question_db['_id']}, question_db)
+                break
+
+    # add variation to new question
+    query = {"_id": ObjectId(message_item.new_response), "is_active": True}
+    doc = {
+        "id": str(uuid.uuid4()),
+        "text": message_item.text,
+        "language": language,
+        "internal": False
+    }
+    result3 = await question_collection.update_one(query,
+                                                   {"$push": {'alternate_questions': doc},
+                                                    "$set": updated_info_query})
+
+    return f"Graded {result1 and result1.modified_count} question. " \
+           f"Removed {result2 and result2.modified_count} variation from question. " \
+           f"Added {result3 and result3.modified_count} variation to question."
--- app/server/routers/grading.py
+++ app/server/routers/grading.py
@@ -2,9 +2,9 @@ from datetime import date
 
 from fastapi import APIRouter, Query
 
-from ..db_utils.grading import get_grading_messages_and_count_db, skip_message_db
+from ..db_utils.grading import get_grading_messages_and_count_db, skip_message_db, update_message_db
 from ..models.current_user import CurrentUserSchema
-from ..models.message import GetMessagesTable, GetGradingsTable, SkipMessage
+from ..models.message import GetGradingsTable, SkipMessage, UpdateMessageResponse
 
 router = APIRouter(
     tags=["gradings"],
@@ -23,7 +23,8 @@ async def get_gradings_message(topic: str = Query(None),
                                since: list[date] = Query(None)):
     messages, total = await get_grading_messages_and_count_db(topic=topic, search_query=search_query,
                                                               accuracy=accuracy, question_status=question_status,
-                                                              current_page=current_page, page_size=page_size, since=since)
+                                                              current_page=current_page, page_size=page_size,
+                                                              since=since)
 
     result = {
         "data": messages,
@@ -35,22 +36,46 @@ async def get_gradings_message(topic: str = Query(None),
 
 @router.delete("/")
 async def skip_questions(message: SkipMessage,
-                           # current_user: CurrentUserSchema = Depends(get_current_active_user)
-                           ):
+                         # current_user: CurrentUserSchema = Depends(get_current_active_user)
+                         ):
     current_user = CurrentUserSchema(**{
-            "username": "user@pand.ai",
-            "userId": "5efdc63e74f7e093ce73db78",
-            "access": "admin",
-            "permissions": [
-                "create_flow",
-                "read_flow",
-            ],
-            "name": "Teh Li heng ",
-            "email": "liheng@pand.ai",
-            "avatar": "https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png",
-            "is_active": True
-        })
+        "username": "user@pand.ai",
+        "userId": "5efdc63e74f7e093ce73db78",
+        "access": "admin",
+        "permissions": [
+            "create_flow",
+            "read_flow",
+        ],
+        "name": "Teh Li heng ",
+        "email": "liheng@pand.ai",
+        "avatar": "https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png",
+        "is_active": True
+    })
     status = await skip_message_db(message, current_user)
+    return {
+        "status": status,
+        "success": True,
+    }
+
+
+@router.patch("/")
+async def update_message_response(message: UpdateMessageResponse,
+                                  # current_user: CurrentUserSchema = Depends(get_current_active_user)
+                                  ):
+    current_user = CurrentUserSchema(**{
+        "username": "user@pand.ai",
+        "userId": "5efdc63e74f7e093ce73db78",
+        "access": "admin",
+        "permissions": [
+            "create_flow",
+            "read_flow",
+        ],
+        "name": "Teh Li heng ",
+        "email": "liheng@pand.ai",
+        "avatar": "https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png",
+        "is_active": True
+    })
+    status = await update_message_db(message, current_user)
     return {
         "status": status,
         "success": True,
