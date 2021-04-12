7b6b031844ef341f5c0e759b35b883cae035b456
liheng <liheng@pand.ai>
2021-04-12 11:05:50 +0800

    change api keys

--- app/server/routers/dashboard.py
+++ app/server/routers/dashboard.py
@@ -23,12 +23,12 @@ async def get_user_message():
     monthly_trend_percentage, monthly_trend_target = await Message.get_monthly_trend()
     res = {
         "data": {
-            "totalUserMessage": await Message.get_total_count(),
+            "total": await Message.get_total_count(),
             "monthlyTrend": monthly_trend_percentage,
             "monthlyTarget": monthly_trend_target,
             "weeklyTrend": weekly_trend_percentage,
             "weeklyTarget": weekly_trend_target,
-            "dailyUserMessage": await Message.get_today_count()
+            "daily": await Message.get_today_count()
         },
         "status": True
     }
@@ -41,12 +41,12 @@ async def get_users():
     monthly_trend_percentage, monthly_trend_target = await User.get_monthly_trend()
     res = {
         "data": {
-            "totalUser": await User.get_total_count(),
+            "total": await User.get_total_count(),
             "monthlyTrend": monthly_trend_percentage,
             "monthlyTarget": monthly_trend_target,
             "weeklyTrend": weekly_trend_percentage,
             "weeklyTarget": weekly_trend_target,
-            "dailyUser": await User.get_today_count()
+            "daily": await User.get_today_count()
         },
         "status": True
     }
@@ -59,12 +59,12 @@ async def get_conversations():
     monthly_trend_percentage, monthly_trend_target = await Conversation.get_monthly_trend()
     res = {
         "data": {
-            "totalConversation": await Conversation.get_total_count(),
+            "total": await Conversation.get_total_count(),
             "monthlyTrend": monthly_trend_percentage,
             "monthlyTarget": monthly_trend_target,
             "weeklyTrend": weekly_trend_percentage,
             "weeklyTarget": weekly_trend_target,
-            "dailyConversation": await Conversation.get_today_count()
+            "daily": await Conversation.get_today_count()
         },
         "status": True
     }
@@ -108,8 +108,8 @@ async def message_trend(since: list[date]):
 
 @router.get("/bottom-part/questions-trend")
 async def trend_question(sort_by: str = Query(None, alias="sortBy")):
-    today = datetime.now()
-    # today = datetime(2020, 4, 12)
+    # today = datetime.now()
+    today = datetime(2020, 4, 12)
     data, total_model, average_model = await question_ranking(today, sorter=sort_by)
     res = {
         "data": {"table": data, "total": total_model, "average": average_model},
